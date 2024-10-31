d7b143b22b476177ad8972836259dab4541aa35f
eliseydudin <dudinelisey630@gmail.com>
2024-10-31 15:51:17 +0300

    tricky_range fix

--- bin/tricky_range.rs
+++ bin/tricky_range.rs
@@ -4,11 +4,11 @@ fn main() {
     vec[0] = 1;
     vec[1] = 1;
 
-    for i in 2..(n + 1) {
+    for i in 2..=n {
         vec[i] = if i % 2 == 0 {
             vec[i / 2] + 1
         } else {
-            vec[(i + 1) / 2] + 1 - vec[i / 2]
+            vec[(i + 1) / 2] + 1 - vec[(i - 1) / 2]
         }
     }
 
